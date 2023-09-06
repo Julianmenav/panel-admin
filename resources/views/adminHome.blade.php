178654228ec0238224cbe29dd4e292f411893f39
Julianmenav <julianmenav2@gmail.com>
2023-09-06 04:07:00 +0200

    fix

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -95,7 +95,7 @@
 @section('js')
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
     <script>
-        document.addEventListener('DOMContentLoaded', function()  
+        document.addEventListener('DOMContentLoaded', function() {
 
             $.ajaxSetup({
                 headers: {
